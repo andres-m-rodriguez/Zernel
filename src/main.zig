e38e432bd7bd8d9f36fa62fceebf451953a840f5
Andres <andresitomanuel@gmail.com>
2026-01-13 01:14:13 -0400

    Add hello world using u16 bits

--- src/main.zig
+++ src/main.zig
@@ -1,10 +1,13 @@
 const hello = "Hello world";
-
+const color:u16 = 0x0F00;
 export fn _start() noreturn {
-    const vga: [*]volatile u8 = @ptrFromInt(0xb8000);
-    for (hello, 0..) |byte, i| {
-        vga[i * 2] = byte;
+    const vga: [*]volatile u16 = @ptrFromInt(0xb8000);
+    for (0..80 * 25) |i| { // Clears the VGA buffer
+        vga[i * 2] = ' ';
         vga[i * 2 + 1] = 0x0b;
+    }
+    for (hello, 0..) |byte, i| {
+        vga[i] = byte | color;
     }
     while (true) asm volatile ("hlt");
 }
